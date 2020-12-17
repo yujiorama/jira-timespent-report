73a9d0a55f0fda3891f1855ea96eea1be7ae01c2
Yuji Okazawa <yujiorama@gmail.com>
2020-12-17 18:24:02 +0900

    chore: http サーバーの終了処理にシグナルの対応を含める

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -2,11 +2,17 @@ package web
 
 import (
 	"bitbucket.org/yujiorama/jira-timespent-report/jira"
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 const (
@@ -34,21 +40,46 @@ func CanDo() bool {
 func Do() {
 	log.Println("start")
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", reportHandler)
 
-	s := &http.Server{
-		Addr:    fmt.Sprintf("%s:%d", host, port),
-		Handler: mux,
+	server := &http.Server{
+		Addr:        fmt.Sprintf("%s:%d", host, port),
+		Handler:     mux,
+		BaseContext: func(_ net.Listener) context.Context { return ctx },
 	}
 
-	err := s.ListenAndServe()
-	if err != nil {
-		log.Fatal("Error Starting the HTTP Server : ", err)
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(
+		signalChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+	)
+	<-signalChan
+
+	gracefulCtx, cancelShutdown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelShutdown()
+
+	if err := server.Shutdown(gracefulCtx); err != nil {
+		log.Printf("shutdown error: %v\n", err)
+		defer os.Exit(1)
 		return
 	}
 
-	log.Println("end")
+	log.Printf("end\n")
+
+	defer os.Exit(0)
+	return
 }
 
 type errorResponse struct {
